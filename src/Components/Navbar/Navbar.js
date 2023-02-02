00c62af515520f12611f24cd6b04b19528a4ebbd
Francine <franmelopepe2@gmail.com>
2023-02-02 16:33:06 +0100

    onClick "button" to download the CV pdf

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,6 +3,17 @@ import { Icon } from "@iconify/react";
 import Logo from "./../../Images/logo_fran_bigger.png";
 
 export default function Navbar() {
+  const onButtonClick = () => {
+    fetch("Francine_Pêpe_CV.pdf").then((response) => {
+      response.blob().then((blob) => {
+        const fileURL = window.URL.createObjectURL(blob);
+        let alink = document.createElement("a");
+        alink.href = fileURL;
+        alink.download = "Francine_Pêpe_CV.pdf";
+        alink.click();
+      });
+    });
+  };
   return (
     <>
       <nav className="navbar">
@@ -11,7 +22,12 @@ export default function Navbar() {
         </a>
         <input type="checkbox" id="toggler" />
         <label htmlFor="toggler">
-          <Icon icon="system-uicons:menu-hamburger" color="#373737" width="50" height="50" />
+          <Icon
+            icon="system-uicons:menu-hamburger"
+            color="#373737"
+            width="50"
+            height="50"
+          />
         </label>
         <div className="menu">
           <ul className="list">
@@ -29,10 +45,11 @@ export default function Navbar() {
             </li>
             <li>
               <a
-                href="https://acrobat.adobe.com/link/review?uri=urn:aaid:scds:US:c4e972e9-8e9f-35bb-b9de-1bef646f3c91"                
+                href="https://acrobat.adobe.com/link/review?uri=urn:aaid:scds:US:c4e972e9-8e9f-35bb-b9de-1bef646f3c91"
                 target="_blank"
                 rel="noreferrer noopener"
                 download
+                onClick={onButtonClick}
               >
                 Resumé
               </a>
